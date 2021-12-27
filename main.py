7fbec2c41b7ed2a06d9642726c32ce0670219a99
neonlaim <neonlaim@gmail.com>
2021-12-27 10:55:35 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# 1. Handle all the exception
+
+# Setup
+actor = {"name": "John Cleese", "rank": "awesome"}
+
+# Function to modify!!!
+def get_last_name():
+    return actor["last_name"]
+
+# Test code
+get_last_name()
+print("All exceptions caught! Good job!")
+print("The actor's last name is %s" % get_last_name())
+
+# 2. Write a Python program to create a function that takes one argument, and that argument will be multiplied with an unknown given number.
+# Sample Output:
+# Double the number of 15 = 30
+# Triple the number of 15 = 45
+# Quadruple the number of 15 = 60
+# Quintuple the number 15 = 75
+#
+# 3. Write a Python program to create a class and display the namespace of the said class.
+#
+# 4. Define a Python function student(). Using function attributes display the names of all arguments.
+#
+# 5. one example each - Arithmetic operator
+# Relational operators
+# Assignment operators
+# Logical operators
+# Membership operators
+# Identity operators
+# Bitwise operators
+#
+# 6. One example of all keywords in Python Programming
